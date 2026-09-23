89e60cb8516e8c0c74e861eb6c78dc808ace5061
raynovastudio <raynovastudio@gmail.com>
2026-09-23 20:22:46 +0100

    fix: send ticket emails from verified conference-wihcn.xyz domain

--- src/server/lib/email.ts
+++ src/server/lib/email.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM_EMAIL = "WIHCN CON III <tickets@wihcn.org>";
+const FROM_EMAIL = "WIHCN CON III <tickets@conference-wihcn.xyz>";
 
 export interface SendConfirmationParams {
   to: string;
